a0c4e508f3d7e12b5561c21f40cb8b3999cc0fb5
Munaa <munaa.tsetsegmaa@gmail.com>
2020-03-04 18:41:55 +0800

    Message button нэмсэн

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const button = document.querySelector("button");
 const output = document.querySelector(".output");
+const buttonMessage = document.querySelector(".button");
+const outputMessage = document.querySelector(".outputMessage");
+const inputMessage = document.querySelector(".inputMessage");
 const tipVat = 0.15;
 
 button.innerHTML = "Hello";
@@ -11,3 +14,7 @@ button.addEventListener("click", function() {
   let temp = `<h1> You should top $${tip} on  $${cost.value}</h1>`;
   output.innerHTML = temp;
 });
+buttonMessage.addEventListener("click", showMessage);
+function showMessage() {
+  outputMessage.innerHTML = ` Hello ${inputMessage.value}`;
+}
